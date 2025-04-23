c4b5f3b681789a4325102b5ac65da51f1d86981d
Ben <benw@student.chalmers.se>
2025-04-23 23:59:37 +0200

    Set starting sequence number back to 0

--- src/application.c
+++ src/application.c
@@ -6,7 +6,7 @@
 App app = {initObject(), initTimer(), initTimer(), .user_button_mode=0, .trigger_mode=0, .inter_arrival_times={}, .tap_count=0, .bounce_flag=0, .rank=NODE_ID, .ranks={}, .network_size=1, .conductor=-1, .evaling_conductor=0, .can_queue={}, .can_queue_start=0, .can_queue_end=MAX_CAN_QUEUE_SIZE-1, .can_queue_size=0, .can_cooldown_active=0, .can_msg_min_interval_ms=1000, .print_can_tx=0};
 
 MusicPlayer music_player = {initObject(), DEFAULT_KEY, DEFAULT_TEMPO, .note_idx=-1, .is_playing=0, .is_led_blinking=0, .nth_note_to_play=0, .current_note_segment=0, .force_mute = 0};
-UserInputHandler userInputHandler = {initObject(), {}, 0, 66, 0};
+UserInputHandler userInputHandler = {initObject(), {}, 0, 0, 0};
 Tone_CTRL tone_ctrl = {initObject(), VOLUME, 0, 1, T_1000_Hz, 0, 0, 0};
 Distortion distortion = {initObject(), 1000, 0};
 Serial sci0 = initSerial(SCI_PORT0, &app, reader);
